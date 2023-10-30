f3a9669aa33a6bc4af89d81027e582ee0294de96
nyezza <yezza.nadhir@gmail.com>
2023-10-30 20:41:41 +0100

    test

--- JS/fundamentals/PredictDestructuring.js
+++ JS/fundamentals/PredictDestructuring.js
@@ -1,4 +1,4 @@
-/*      probleme 1 */
+/*      problem 1 */
 const cars = ['Tesla', 'Mercedes', 'Honda']
 const [ randomCar ] = cars
 const [ ,otherRandomCar ] = cars
@@ -17,6 +17,64 @@ const employee = {
 }
 const { name: otherName } = employee;
 //Predict the output
-console.log(name);
+//console.log(name);
+// => error
 console.log(otherName);
+// => Elon
+
+/*      Problem 3     */
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+
+//Predict the output
+console.log(password);
+// => 12345
+console.log(hashedPassword);
+// unknown variable password in person 
+
+/*     Problem 4    */
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+
+//Predict the output
+
+console.log(first == second);
+/* first = 2 ; second = 5
+so 2 != 5 => false */
+
+console.log(first == third);
+/* first = 2 ; third = 2
+so 2 = 2 => True */
+
+
+/*      Problem 5     */
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+
+
+//?--------------Func
+
+
+
 
