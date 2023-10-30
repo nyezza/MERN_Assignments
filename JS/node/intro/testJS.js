db13a5c8eb4fd85dd8fea5aa311226a9fdbf7778
nyezza <yezza.nadhir@gmail.com>
2023-10-30 15:41:59 +0100

    test

--- JS/node/intro/testJS.js
+++ JS/node/intro/testJS.js
@@ -1,2 +1,19 @@
 console.log('hello')
 console.log('test 2')
+const x= 1
+const y= 2
+console.log(x+y)
+
+var beatles = ['Paul', 'George', 'John', 'Ringo'];
+function printNames(names) {
+  function actuallyPrintingNames() {
+    for (var index = 0; index < names.length; index++) {
+      var name = names[index];
+  
+      console.log(name + ' was found at index ' + index);
+    }
+    console.log('name and index after loop is ' + name + ':' + index);
+  }
+  actuallyPrintingNames();
+}
+printNames(beatles);
