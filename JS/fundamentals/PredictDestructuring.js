b1abcaa9ef11583e4873e76fa89f59681b2d47c4
nyezza <yezza.nadhir@gmail.com>
2023-10-30 17:27:30 +0100

    predictDestructing

---
+++ JS/fundamentals/PredictDestructuring.js
@@ -0,0 +1,22 @@
+/*      probleme 1 */
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+// => Tesla
+console.log(otherRandomCar)
+// => Mercedes
+
+/*     prblem 2 */
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+
