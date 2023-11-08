2925b35bc110a4b483b14455eb83a747919014cb
nyezza <yezza.nadhir@gmail.com>
2023-11-08 16:57:06 +0100

    first_jsx

---
+++ React/Practice/fisrt_jsx/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <> 
+    <div className="testCSS">
+      <h1>Hello Dojo!</h1>
+      <h2> things I need to do:</h2>
+      <li> Learn React</li>
+      <li> Climb Mt. Everest</li>
+      <li> Run a marathon</li>
+      <li> Feed the dogs</li>
+    </div>
+      
+    </>
+  );
+}
+
+export default App;
