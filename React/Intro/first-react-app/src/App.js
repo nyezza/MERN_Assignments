f445a837ac020457da773dd86230fd627bcf59e8
nyezza <yezza.nadhir@gmail.com>
2023-11-10 11:35:09 +0100

    onclick

--- React/Intro/first-react-app/src/App.js
+++ React/Intro/first-react-app/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -14,9 +14,11 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          
         >
-          Learn React
+          Learn React 
         </a>
+        <button onClick={()=> alert("this button has been clicked!")}>click on Me</button>
       </header>
     </div>
   );
