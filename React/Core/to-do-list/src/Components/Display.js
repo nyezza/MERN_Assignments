6d3766e8d23d42c4cc56f85966b5e50ff0010962
nyezza <yezza.nadhir@gmail.com>
2023-11-20 14:13:26 +0100

    to-do-list

---
+++ React/Core/to-do-list/src/Components/Display.js
@@ -0,0 +1,24 @@
+const Display = (props) => {
+
+    //Destructure props object
+    const { todoList, handleCompleted, deleteButton } = props;
+  
+  
+    return (
+      <>
+  
+        {
+        todoList.map((todo, index) => (
+          <div key={todo.id}>
+            <p className={todo.isComplete? "completed" : ""}>{todo.content}</p>
+            <input type="checkbox" onChange={() => handleCompleted(todo)} />
+            <button onClick={() => deleteButton(todo.id)}>Delete</button>
+          </div>
+        ))
+        }
+  
+      </>
+    );
+  };
+  
+  export default Display;
