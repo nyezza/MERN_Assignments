3c8945e282e69b4be7cf2e82b84f593226c6e184
nyezza <yezza.nadhir@gmail.com>
2023-11-20 17:21:59 +0100

    apiPokemon

--- React/Practice/Pokemon Api/pokemon-api/src/App.js
+++ React/Practice/Pokemon Api/pokemon-api/src/App.js
@@ -4,25 +4,25 @@ import './App.css';
 function App() {
   const [pokemon , setPokemon]= useState([])
   useEffect(()=>{
-    fetch('https://pokeapi.co/api/v2/pokemon?offset=20&limit=20')
+    fetch('https://pokeapi.co/api/v2/pokemon')
       .then (response =>{
         console.log(response) 
         return response.json})
       .then (response =>{
         console.log(response);
-        setPokemon(response.results)
-        console.log(response.results)})
+        setPokemon(response)
+        console.log(response)})
       .catch (err => console.log(err))
     
   },[])
   
   return (
     <div className="App">
-     {
+    {
       pokemon.map((p,i)=>{
         return {p}
       })
-     }
+    }
     </div>
   )
 }
