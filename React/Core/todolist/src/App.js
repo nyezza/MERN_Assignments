914f8a2787f4d6802525bc057a2acf0b81726eb5
nyezza <yezza.nadhir@gmail.com>
2023-11-20 07:05:37 +0100

    ToDoList

--- React/Core/todolist/src/App.js
+++ React/Core/todolist/src/App.js
@@ -1,12 +1,13 @@
 import React,{useState} from 'react'
 import './App.css';
-cosnt [toDoList , setTodoList] = useState([]);
+
+function App() {
+  const [toDoList , setTodoList] = useState([]);
 
 const updateToDoList = (newtoDo)=>{
   setTodoList=((oldToDo)=>[...oldToDo,newtoDo]);
 }
 
-function App() {
   return (
     <div className="App">
       
