85d4f05b4255015e6abe9cd67aa5e6b7e732e633
nyezza <yezza.nadhir@gmail.com>
2023-11-21 14:19:42 +0100

    fetch api

--- React/Intro/sample-useeffect/src/App.js
+++ React/Intro/sample-useeffect/src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 
 function App () {
     const [heroes , setHeroes] = useState([]);
-    useEffect=(()=> {
-        fetch("https://akabab.github.io/superhero-api/api/all.json")
-        .then(her=>{
-            console.log(her)
-        })
+    useEffect(()=> {
+        Axios.get("https://akabab.github.io/superhero-api/api/all.json")
+        // .then(her=>{
+        //     console.log(her.data)
+        // })
         .then(her =>{
-            console.log(her);
-            setHeroes(her)
-            console.log(her);
+            console.log(her.data);
+            setHeroes(her.data)
+            console.log(her.data);
         })
         .catch(err=> console.log(err))
     }, [])
--- React/Practice/Pokemon Api/pokemon-api/src/App.js
+++ React/Practice/Pokemon Api/pokemon-api/src/App.js
@@ -1,28 +1,30 @@
 import React, { useEffect, useState } from 'react'
 import './App.css';
-
 function App() {
-  const [pokemon , setPokemon]= useState([])
+  const [pokemon , setPokemon] = useState([])
   useEffect(()=>{
-    fetch('https://pokeapi.co/api/v2/pokemon')
-      .then (response =>{
-        console.log(response) 
-        return response.json})
-      .then (response =>{
-        console.log(response);
-        setPokemon(response)
-        console.log(response)})
-      .catch (err => console.log(err))
-    
-  },[])
+    fetch("https://pokeapi.co/api/v2/pokemon")
+    .then(pok => {
+      console.log(pok);
+      return pok.json()
+    })
+    .then(pok => {
+      console.log(pok.results);
+      setPokemon(pok.results);
+      console.log(pok.results);
+    })
+    .catch(err => console.log(err))
+  })
   
   return (
     <div className="App">
-    {
-      pokemon.map((p,i)=>{
-        return {p}
-      })
-    }
+      {
+        pokemon.map((p,index)=>{
+          return(
+            <li key={index}>{p.name}</li>
+          )
+        })
+      }
     </div>
   )
 }
